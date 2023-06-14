b0df8faf7786f993f56afe547fea84aceae22f08
Narnianknight <83618342+Narnianknight@users.noreply.github.com>
2023-06-13 22:41:45 -0400

    Add ability to rebind drop stack modifier key

---
+++ src/main/java/com/minenash/rebind_all_the_keys/mixin/MinecraftClientMixin.java
@@ -0,0 +1,19 @@
+package com.minenash.rebind_all_the_keys.mixin;
+
+import com.minenash.rebind_all_the_keys.RebindAllTheKeys;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import static com.minenash.rebind_all_the_keys.RebindAllTheKeys.DROP_STACK;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+
+    @Redirect(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;hasControlDown()Z"))
+    private boolean handleInputEvents() {
+        return InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), RebindAllTheKeys.getKeyCode(DROP_STACK));
+    }
+}
