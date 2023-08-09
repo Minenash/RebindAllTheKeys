6ef98f47d13387dbe269a5c2a65016e837421438
Jakob <minenash@protonmail.com>
2023-08-09 15:43:39 -0400

    removed unused import

--- src/main/java/com/minenash/rebind_all_the_keys/mixin/MinecraftClientMixin.java
+++ src/main/java/com/minenash/rebind_all_the_keys/mixin/MinecraftClientMixin.java
@@ -3,7 +3,6 @@ package com.minenash.rebind_all_the_keys.mixin;
 import com.minenash.rebind_all_the_keys.RebindAllTheKeys;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.util.InputUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
