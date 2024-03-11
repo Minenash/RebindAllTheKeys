5d9b28402b7c8d69819a64a132b26861d0e3f4bd
Jakob <minenash@protonmail.com>
2024-03-10 20:50:57 -0400

    Finished Fly keybind

--- src/main/java/com/minenash/rebind_all_the_keys/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/minenash/rebind_all_the_keys/mixin/ClientPlayerEntityMixin.java
@@ -2,20 +2,28 @@ package com.minenash.rebind_all_the_keys.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.minenash.rebind_all_the_keys.RebindAllTheKeys;
+import com.mojang.authlib.GameProfile;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
-public abstract class ClientPlayerEntityMixin {
+public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
 
     @Shadow @Final protected MinecraftClient client;
+
+    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
+        super(world, profile);
+    }
 
     @Redirect(method = "tickMovement", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/network/ClientPlayerEntity;setSprinting(Z)V"))
     private void disableDoubleTapSprint(ClientPlayerEntity instance, boolean sprinting) {
@@ -34,8 +42,28 @@ public abstract class ClientPlayerEntityMixin {
     }
 
     @ModifyExpressionValue(method = "tickMovement", at = @At(value = "FIELD", ordinal = 2, target = "Lnet/minecraft/client/input/Input;jumping:Z"))
-    public boolean disableDoubleTapFly(boolean original) {
+    public boolean disableDoubleTapFlyAndProcessJumpButton(boolean original) {
+
+        boolean flying = getAbilities().flying;
+        while (RebindAllTheKeys.FLY.wasPressed()) {
+            getAbilities().flying = !getAbilities().flying;
+        }
+        boolean newFlying = getAbilities().flying;
+        if (newFlying != flying) {
+            bl8 = true;
+            this.sendAbilitiesUpdate();
+            if (newFlying)
+                setPosition(getX(), getY() + 0.25, getZ());
+        }
+
         return RebindAllTheKeys.doubleTapFly.getValue() && original;
     }
 
+    @Unique
+    private static boolean bl8 = false;
+
+    @ModifyExpressionValue(method = "tickMovement", at = @At(value = "FIELD", ordinal = 3, target = "Lnet/minecraft/client/input/Input;jumping:Z"))
+    public boolean correctBl8(boolean original) {
+        return original && !bl8;
+    }
 }
